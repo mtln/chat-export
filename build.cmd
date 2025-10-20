4a9c4a3f92e71c48de50602140e0f205028b0ef0
Andri Krämer <akraemer@matlon.ch>
2025-10-20 19:29:32 +0200

    build.cmd adjusted for new package structure

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
-pyinstaller --noconfirm --onefile --console --name "chat-export" --optimize "2"  "main.py"
+pyinstaller --noconfirm --onefile --console --name "chat-export" --optimize "2"  "chat_export/chat_export.py"
 copy dist\chat-export.exe chat-export.exe
 rm -rf dist
 rm -rf build
